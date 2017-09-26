94a159c4ec62a18a1516336b090edbf036ef6d09
melkotoury <melkotoury@gmail.com>
2017-09-26 17:14:24 +0200

    content: add hardcoded content

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -23,11 +23,11 @@ ta                  <span className='price'>EGP 200</span>
         <Item>
 
             <Item.Content>
-                <Item.Header>French Fries</Item.Header>
+                <Item.Header>Nacho Chips & Salsa</Item.Header>
                 <Item.Meta  >
                     <span className='price'>EGP 200</span>
                 </Item.Meta>
-                <Item.Description>Custom premium cut by farm frites. Add melted cheese for 7LE - chili con carne for 9LE</Item.Description>
+                <Item.Description>Homemade crispy nacho chips served with fresh salsa dip</Item.Description>
             </Item.Content>
             <Item.Content >
                 <ActionButton/>
