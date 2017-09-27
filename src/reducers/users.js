b622a524c03630ee6ad418da60150781f18199fd
melkotoury <melkotoury@gmail.com>
2017-09-27 02:33:42 +0200

    feat: add reducers for users and menu category and menu item

---
+++ src/reducers/users.js
@@ -0,0 +1,21 @@
+
+export default function () {
+    return [
+        {
+            id: 1,
+            username: "melkotoury",
+            name:"Mahmoud El Kotoury",
+            description: "Mahmoud is Software Developer",
+            thumbnail: "http://68.media.tumblr.com/avatar_7519b28ccc55_128.png",
+            role:'dev'
+        },
+        {
+            id: 1,
+            username: "elmenus",
+            name:"ElMenus Admin",
+            description: "ElMenus Admin is the Admin of this app they can add , edit , delete anything in the app",
+            thumbnail: "https://scontent-amt2-1.cdninstagram.com/t51.2885-19/10369265_479529652181504_45833849_a.jpg",
+            role:'admin'
+        }
+    ]
+}
