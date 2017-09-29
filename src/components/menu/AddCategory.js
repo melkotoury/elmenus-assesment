e169ad656883113698a5d77522ed43e8f22c212c
melkotoury <melkotoury@gmail.com>
2017-09-29 23:00:50 +0200

    feat: add category data

--- src/components/menu/AddCategory.js
+++ src/components/menu/AddCategory.js
@@ -1,65 +1,45 @@
 import React , {Component} from 'react'
 import { Grid, Button, Divider, Form } from 'semantic-ui-react'
 import '../../stylesheets/AddCategory.css'
-
-
-
 class AddCategory extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-            nameValue: '',
-            descriptionValue:''
-        };
-
-        this.handleNameChange = this.handleNameChange.bind(this);
-        this.handleDescriptionChange = this.handleDescriptionChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleClick = this.handleClick.bind(this);
     }
-
-    handleNameChange(event) {
-        this.setState({nameValue: event.target.nameValue});
-    }
-    handleDescriptionChange(event) {
-        this.setState({descriptionValue: event.target.descriptionValue});
-    }
-    handleSubmit(event) {
-        alert(this.state.nameValue);
-        console.log(this.state.descriptionValue);
+    handleClick(event) {
+        alert('A name was submitted: ' + this.category_name.value);
+        console.log('A description ' + this.category_description.value);
         event.preventDefault();
     }
-
-    render () {
-        return (
-            <div className='createCategoryForm'>
-
-
-                <Form size='tiny' key='tiny' onSubmit={this.handleSubmit}>
-                    <Grid>
-                        <Grid.Row columns={3}>
-
-                            <Grid.Column>
-                                <Form.Field value={this.state.nameValue} type='text' onChange={this.handleNameChange}  control='input' placeholder='English Name' />
-                            </Grid.Column>
-                            <Grid.Column>
-                                <Form.Field value={this.state.descriptionValue} type='text' onChange={this.handleDescriptionChange}  control='input' placeholder='English Description' />
-                            </Grid.Column>
-                            <Grid.Column>
-                                <Button positive fluid type="submit"> Create Category</Button>
-                            </Grid.Column>
-
-
-                        </Grid.Row>
-                    </Grid>
-
-
-
-                    <Divider hidden />
-                </Form>
-
-            </div>
-        );
-    }
-
+    render() {
+            return (
+                <div className='createCategoryForm'>
+                    <Form size='tiny' key='tiny' >
+                        <Grid>
+                            <Grid.Row columns={3}>
+                                <Grid.Column>
+                                    <input 
+                                        ref={(category_name) => this.category_name = category_name} 
+                                        type='text'   
+                                        placeholder='English Name' 
+                                    />
+                                 </Grid.Column>
+                                 <Grid.Column>
+                                    <input 
+                                        ref={(category_description) => this.category_description = category_description} 
+                                        type='text'  
+                                        placeholder='English Name'
+                                     />
+                                 </Grid.Column>
+                                 <Grid.Column>
+                                    <Button positive fluid  onClick={this.handleClick}>Create Category</Button>
+                                 </Grid.Column>
+                            </Grid.Row>
+                        </Grid>
+                        <Divider hidden />
+                    </Form>
+                </div>
+    );
+}
 }
 export default AddCategory
