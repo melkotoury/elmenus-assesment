92307df8e7d5e7772c24b10e4ff8754462a6d697
melkotoury <melkotoury@gmail.com>
2017-10-01 02:51:31 +0200

    refact: refactor redux directories

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,6 @@
 import {combineReducers} from 'redux'
 
 import UserReducer from './users'
-import MenuReducer from './menu_category'
 
 /**
 * Take all our reducers and combine it in one javascript object to be passed to the store
